2e590986030e4d06778f9cb82ab58b6207373258
martin-alem <alemajohmartin@gmail.com>
2021-07-26 11:08:48 -0400

    implemented Friend component

---
+++ src/view/Friend/Friend.js
@@ -0,0 +1,33 @@
+import React, { PureComponent } from 'react';
+import avatar from "../../img/avatar.jpg";
+import "./Friend.css";
+
+class Friend extends PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="Friend">
+                <div className="Container">
+                    <div className="InnerContainer">
+                        <div className="Image">
+                            <img src={avatar} alt="User image" className="Avatar" />
+                        </div>
+                        <div className="Title">
+                            <h3>Adjyannah West</h3>
+                            <p>I am using mChat</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Friend;
